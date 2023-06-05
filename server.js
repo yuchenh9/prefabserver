3c12ecf587d2c7f9931d4a8ecfd8b306a523849c
Robinson Cruisso <yuchenh9lsb@icloud.com>
2023-06-05 11:29:42 -0500

    more

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 // Use cors middleware to enable cors
 app.use(cors());
+app.use((req, res, next) => {
+  console.log(`New connection - Method: ${req.method}, URL: ${req.url}`);
+  next();
+});
 
 app.use(express.static('AssetBundles'));  // Serve files from the AssetBundles directory
 
